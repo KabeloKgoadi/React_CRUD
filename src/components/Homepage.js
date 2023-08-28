3c9dbdab860385078fa3536dc15a5ff872e4352e
Kabelo Kgoadi <99649551+KabeloKgoadi@users.noreply.github.com>
2023-08-28 09:45:02 +0200

    Update Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+
 
 const Homepage = () => {
-    const data = Object.keys(localStorage);
-    let navigate = useNavigate();
+    const data = Object.keys(localStorage)
+
 
     function handleDelete (id)
     {
@@ -12,24 +13,49 @@ const Homepage = () => {
     }
     
   return (
-    <>
-        <button><Link to={`/create`}>Add Members</Link></button>
-      {data.map((value,id) => {
-        const user = JSON.parse(localStorage.getItem(value));
-        console.log(user)
-        return (
-            <div key={id}>
-             { console.log(user.name)}
-            <h1>{user.name}</h1>
-            <p>{user.title}</p>           
-            <button ><Link to={`/update/${id}`}>Update</Link></button>
-            <button onClick={() => handleDelete(id)}>Delete</button>
-            </div>     
-                       
-        )
-      })}
+    <body>
       
-    </>
+        <button className='btnAddHome'><Link to={`/create`}>Add Member</Link></button>
+        
+        <br />
+        <br />
+        {localStorage.length !==0 ? (
+            data.map((value,id) => {
+              const user = JSON.parse(localStorage.getItem(value))
+              return (
+                  <div key={id}>
+                    
+                    <div className='body-container'>
+                      <div className='body-picture'>
+                          <img className='display-picture' src={user.file} alt="Uploaded Picture"  width = "90px"/>
+                      </div>
+                        <div className='body-items'>
+                            <h2>{user.name}</h2>
+                            <h6>{user.title}</h6>
+                        </div>
+
+                        <div className='member__edit'>
+                          <Link to={`/update/${id}`}> <i className='fas fa-pen'></i></Link>
+                          <i className='fas fa-trash' onClick={() => handleDelete(id)}></i>
+                        </div>
+                       
+                    </div>
+                    <br />
+                    <br />
+                    <br />
+                  </div>     
+                             
+              )
+              
+            })
+        ) : (
+          <div className='member-null'>
+            <p>No Members in the database</p>
+          </div>
+        )}
+       
+      
+    </body>
   )
 }
 
